c4ab50bd9b03c9e0989ccc4d444f15dae54ef432
Justin Lange <well1912@gmail.com>
2016-09-05 12:34:14 -0700

    Add docstrings to tests

--- elections_r_us/security.py
+++ elections_r_us/security.py
@@ -2,6 +2,10 @@ from models import User
 from passlib.apps import custom_app_context as pwd_context
 
 def create_user(session, username, password):
+    """Add a new user to the database.
+
+    session is expected to be a dbsession, username and password are
+    expected to be (unencrypted) unicode strings."""
     session.add(User(
         username=username,
         password=pwd_context.encrypt(password)
@@ -9,6 +13,9 @@ def create_user(session, username, password):
 
 
 def check_login(session, username, password):
+    """Return whether username and password match in the database.
+
+    If username is not present, returns False."""
     try:
         query = session.query(User).filter(User.username == username).first()
         return pwd_context.verify(password, query.password)
--- elections_r_us/tests/test_security.py
+++ elections_r_us/tests/test_security.py
@@ -4,22 +4,26 @@ from ..models import User
 
 
 def test_user_gets_created(new_session):
+    """Test that after create_user adds to the database."""
     from ..security import create_user
     create_user(new_session, 'username', 'password')
     assert len(new_session.query(User).all()) == 1
 
 
 def test_user_can_login(session_with_user):
+    """Test that check_login returns True with the correct login."""
     from ..security import check_login
     assert check_login(*session_with_user)
 
 
 def test_nonexistent_user_login_fails(new_session):
+    """Test check_login returns False when the username isn't present."""
     from ..security import check_login
     assert not check_login(new_session, 'hello', 'world')
 
 
 def test_bad_password_login_fails(session_with_user):
+    """Test check_login returns False when the password doesn't match."""
     from ..security import check_login
     session, username, password = session_with_user
     assert not check_login(session, username, password + 'not!')
