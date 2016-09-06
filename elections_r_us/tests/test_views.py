ea60830a61729fb6f53519cfe88b0300561ecfae
Justin Lange <well1912@gmail.com>
2016-09-06 11:25:33 -0700

    Implement change password view
    Factor out verify_password

--- elections_r_us/tests/test_views.py
+++ elections_r_us/tests/test_views.py
@@ -12,6 +12,14 @@ VALID_LOGINS = [
 ]
 
 
+VALID_PASSWORDS = [
+    'password',
+    'eoae8uadh%$  ',
+    'adiug98',
+    '8419tagsh'
+]
+
+
 BAD_USERNAMES = [
     '',
     'user name',
@@ -122,7 +130,6 @@ def test_register_view_success_creates_user(new_session, username, password):
     assert new_session.query(User).first().username == username
 
 
-
 @pytest.mark.parametrize('username', BAD_USERNAMES)
 def test_register_view_bad_username(new_session, username):
     from ..views.default import register_view
@@ -154,3 +161,76 @@ def test_register_view_unmatched_passwords(new_session, p1, p2):
         'password_confirm': p2
     }))
     assert register_results == {'unmatched_password': True}
+
+
+@pytest.mark.parametrize('new_password', VALID_PASSWORDS)
+def test_password_reset_success(session_with_user, new_password):
+    from ..views.default import password_reset_view
+    session, username, old_password = session_with_user
+    result = password_reset_view(dummy_post_request(session, {
+        'username': username,
+        'password': new_password,
+        'password_confirm': new_password
+    }))
+    assert 'password_reset' in result
+
+
+@pytest.mark.parametrize('new_password', VALID_PASSWORDS)
+def test_password_reset_updates_password(session_with_user, new_password):
+    from ..views.default import password_reset_view
+    from ..security import pwd_context
+    from ..models import User
+    session, username, old_password = session_with_user
+    password_reset_view(dummy_post_request(session, {
+        'username': username,
+        'password': new_password,
+        'password_confirm': new_password
+    }))
+    assert pwd_context.verify(
+        new_password,
+        session.query(User).filter(User.username == username).first().password
+    )
+
+
+@pytest.mark.parametrize('new_password', BAD_PASSWORDS)
+def test_password_reset_bad_password(session_with_user, new_password):
+    from ..views.default import password_reset_view
+    session, username, old_password = session_with_user
+    response = password_reset_view(dummy_post_request(session, {
+        'username': username,
+        'password': new_password,
+        'password_confirm': new_password
+    }))
+    assert 'bad_password' in response
+
+
+@pytest.mark.parametrize('p1, p2', UNMATCHED_PASSWORDS)
+def test_password_reset_unmatched_password(session_with_user, p1, p2):
+    from ..views.default import password_reset_view
+    session, username, old_password = session_with_user
+    response = password_reset_view(dummy_post_request(session, {
+        'username': username,
+        'password': p1,
+        'password_confirm': p2
+    }))
+    assert 'unmatched_password' in response
+
+
+@pytest.mark.parametrize('password', VALID_PASSWORDS)
+def test_password_verify_success(password):
+    from ..views.default import verify_password
+    assert verify_password(password, password)
+
+
+@pytest.mark.parametrize('password', BAD_PASSWORDS)
+def test_password_verify_bad(password):
+    from ..views.default import verify_password, BadPassword
+    with pytest.raises(BadPassword):
+        verify_password(password, password)
+
+
+@pytest.mark.parametrize('p1, p2', UNMATCHED_PASSWORDS)
+def test_password_verify_unmatched(p1, p2):
+    from ..views.default import verify_password, UnmatchedPassword
+    with pytest.raises(UnmatchedPassword):
+        verify_password(p1, p2)
--- elections_r_us/views/default.py
+++ elections_r_us/views/default.py
@@ -3,7 +3,7 @@ from pyramid.view import view_config
 from pyramid.security import remember, forget
 
 from ..models import User
-from ..security import check_login, create_user
+from ..security import check_login, create_user, change_password
 
 
 class BadUsername(Exception):
@@ -18,17 +18,21 @@ class UnmatchedPassword(Exception):
     pass
 
 
+def verify_password(password, password_confirm):
+    if len(password) < 6:
+        raise BadPassword()
+    if password != password_confirm:
+        raise UnmatchedPassword()
+    return True
+
+
 def verify_registration(username, password, password_confirm):
     if username == '':
         raise BadUsername()
     for c in username:
         if c != '_' and not c.isalnum():
             raise BadUsername()
-    if len(password) < 6:
-        raise BadPassword()
-    if password != password_confirm:
-        raise UnmatchedPassword()
-    return True
+    return verify_password(password, password_confirm)
 
 
 def user_exists(session, username):
@@ -74,5 +78,21 @@ def register_view(request):
         except UnmatchedPassword:
             return {'unmatched_password': True}
         create_user(request.dbsession, username, password)
-        return HTTPFound('/')
+        return HTTPFound(location='/')
     return {}
+
+
+def password_reset_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirm = request.POST['password_confirm']
+        change_password(request.dbsession, username, password)
+        try:
+            verify_password(password, password_confirm)
+        except BadPassword:
+            return {'bad_password': True}
+        except UnmatchedPassword:
+            return {'unmatched_password': True}
+        return {'password_reset': True}
+
