027959b9306f9fc581aba20f332f4d2488369d53
clessor88 <clessor88@yahoo.com>
2016-09-05 17:34:19 -0700

    more tests! checks for bad username/password

--- elections_r_us/tests/test_views.py
+++ elections_r_us/tests/test_views.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 import pytest
+from pyramid.httpexceptions import HTTPFound
+from pyramid import testing
 
 
 VALID_LOGINS = [
@@ -58,3 +60,84 @@ def test_unmatched_password(p1, p2):
     username = 'username'
     with pytest.raises(UnmatchedPassword):
         verify_registration(username, p1, p2)
+
+
+def test_existent_user(session_with_user):
+    from ..views.default import user_exists
+    session, username, password = session_with_user
+    assert user_exists(session, username)
+
+
+def test_nonexistent_user(new_session):
+    from ..views.default import user_exists
+    username = 'username'
+    assert not user_exists(new_session, username)
+
+
+def dummy_post_request(new_session, params):
+    dummy = testing.DummyRequest(post=params)
+    dummy.dbsession = new_session
+    return dummy
+
+
+def test_login_view_success(session_with_user):
+    from ..views.default import login_view
+    session, username, password = session_with_user
+    login_results = login_view(dummy_post_request(session, {
+        'username': username,
+        'password': password
+    }))
+    assert isinstance(login_results, HTTPFound)
+
+
+def test_login_view_failure(new_session):
+    from ..views.default import login_view
+    login_results = login_view(dummy_post_request(new_session, {
+        'username': 'username',
+        'password': 'password'
+    }))
+    assert login_results == {'login_failure': True}
+
+
+@pytest.mark.parametrize('username, password', VALID_LOGINS)
+def test_register_view_success(new_session, username, password):
+    from ..views.default import register_view
+    register_results = register_view(dummy_post_request(new_session, {
+        'username': username,
+        'password': password,
+        'password_confirm': password
+    }))
+    assert isinstance(register_results, HTTPFound)
+
+
+@pytest.mark.parametrize('username', BAD_USERNAMES)
+def test_register_view_bad_username(new_session, username):
+    from ..views.default import register_view
+    register_results = register_view(dummy_post_request(new_session, {
+        'username': username,
+        'password': 'password',
+        'password_confirm': 'password'
+    }))
+    assert register_results == {'bad_username': True}
+
+
+@pytest.mark.parametrize('password', BAD_PASSWORDS)
+def test_register_view_bad_password(new_session, password):
+    from ..views.default import register_view
+    register_results = register_view(dummy_post_request(new_session, {
+        'username': 'username',
+        'password': password,
+        'password_confirm': password
+    }))
+    assert register_results == {'bad_password': True}
+
+
+@pytest.mark.parametrize('p1, p2', UNMATCHED_PASSWORDS)
+def test_register_view_unmatched_passwords(new_session, p1, p2):
+    from ..views.default import register_view
+    register_results = register_view(dummy_post_request(new_session, {
+        'username': 'username',
+        'password': p1,
+        'password_confirm': p2
+    }))
+    assert register_results == {'unmatched_password': True}
--- elections_r_us/views/default.py
+++ elections_r_us/views/default.py
@@ -1,8 +1,6 @@
-from pyramid.response import Response
-
-
-def my_view(request):
-    return Response('hi')
+from ..models import User
+from pyramid.httpexceptions import HTTPFound
+from ..security import check_login
 
 
 class BadUsername(Exception):
@@ -28,3 +26,33 @@ def verify_registration(username, password, password_confirm):
     if password != password_confirm:
         raise UnmatchedPassword()
     return True
+
+
+def user_exists(session, username):
+    query = session.query(User).filter(User.username == username).all()
+    return len(query) > 0
+
+
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    if check_login(request.dbsession, username, password):
+        return HTTPFound('/')
+    else:
+        return {'login_failure': True}
+
+
+def register_view(request):
+    try:
+        verify_registration(
+            request.POST['username'],
+            request.POST['password'],
+            request.POST['password_confirm']
+        )
+    except BadUsername:
+        return {'bad_username': True}
+    except BadPassword:
+        return {'bad_password': True}
+    except UnmatchedPassword:
+        return {'unmatched_password': True}
+    return HTTPFound('/')
