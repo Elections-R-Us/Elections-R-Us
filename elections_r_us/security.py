3c3f6c9584b43709aeafa061183182bf796d2e0a
Justin Lange <well1912@gmail.com>
2016-09-07 12:09:17 -0700

    Write tests, start implementing favoritecandidates

--- elections_r_us/security.py
+++ elections_r_us/security.py
@@ -33,6 +33,8 @@ def create_user(session, username, password):
 
 def change_password(session, username, new_password):
     user = session.query(User).filter(User.username == username).first()
+    if user is None:
+        import pdb; pdb.set_trace()
     user.password = pwd_context.encrypt(new_password)
 
 
--- elections_r_us/tests/test_model.py
+++ elections_r_us/tests/test_model.py
@@ -1,29 +1,61 @@
 """Test models."""
 
 from __future__ import unicode_literals
+import pytest
 
-from ..models import User
+from ..models import User, FavoriteCandidate
 
 
-def test_user_gets_added(new_session):
+@pytest.fixture
+def test_user():
+    return User(username='user', password=b'password')
+
+
+def test_user_gets_added(new_session, test_user):
     """Test user gets added to database."""
-    model = User(username='user', password=b'password')
-    new_session.add(model)
+    new_session.add(test_user)
     new_session.flush()
-    assert(len(new_session.query(User).all()) == 1)
+    assert len(new_session.query(User).all()) == 1
 
 
-def test_user_stores_username(new_session):
+def test_user_stores_username(new_session, test_user):
     """Test user's username gets added to database."""
-    model = User(username='user', password=b'password')
-    new_session.add(model)
+    new_session.add(test_user)
     new_session.flush()
-    assert(new_session.query(User).first().username == 'user')
+    assert new_session.query(User).first().username == test_user.username
 
 
-def test_user_stores_password(new_session):
+def test_user_stores_password(new_session, test_user):
     """Test user's password gets added to database."""
-    model = User(username='user', password=b'password')
-    new_session.add(model)
+    new_session.add(test_user)
     new_session.flush()
-    assert(new_session.query(User).first().password == b'password')
+    assert new_session.query(User).first().password == test_user.password
+
+
+def make_test_candidate(userid):
+    return FavoriteCandidate(
+        userid=userid,
+        candidatename='Gary Johnson / Bill Weld',
+        party='Libertarian party',
+        office='President/Vice President',
+        website='http://www.johnsonweld.com',
+        email='info@johnsonweld.com',
+        phone='801-303-7922'
+    )
+
+
+FAVORITE_CANDIDATE_FIELDS = [
+    'userid', 'candidatename', 'party', 'office', 'website', 'email', 'phone'
+]
+
+
+@pytest.mark.parametrize('field', FAVORITE_CANDIDATE_FIELDS)
+def test_candidate_stores_field(session_with_user, field):
+    """Test candidate gets added to a user's favorites."""
+    session, username, password = session_with_user
+    userid = session.query(User).filter(User.username == username).first().id
+    test_candidate = make_test_candidate(userid)
+    session.add(test_candidate)
+    session.flush()
+    stored_model = session.query(FavoriteCandidate).first()
+    assert getattr(stored_model, field) == getattr(test_candidate, field)
